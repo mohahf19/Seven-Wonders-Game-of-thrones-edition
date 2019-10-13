d9f1eb3c4f40c9af52f58615277751bfa64ddf69
makman <muhammadarhamkhan@gmail.com>
2019-10-13 12:56:18 +0300

    added socket code

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,26 @@
+import connection.models.Client;
+import connection.models.Server;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println( "starts");
+                Client client = new Client("127.0.0.1", 5000);
+            }
+        }, 2000);
+
+        Server server = new Server(5000);
+
+
+
     }
+
 }
