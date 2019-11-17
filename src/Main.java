ca5618dadfcffaceb59c3087fd30456dcb6e0720
Mohamad Fakhouri <mohahf19@gmail.com>
2019-11-18 02:29:21 +0300

    Wait Screen

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/ui/MainMenu.fxml"));
         primaryStage.setTitle("Seven Houses");
-        primaryStage.setScene(new Scene(root, 1080, 700));
+        primaryStage.setScene(new Scene(root, 1280, 800));
         primaryStage.show();
 
         new Server();
