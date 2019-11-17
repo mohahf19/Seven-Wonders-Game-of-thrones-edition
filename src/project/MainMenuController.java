f159d80a3be2810bd74e78dd4c7e23accd6d038f
mohahf19 <mohahf19@gmail.com>
2019-11-17 23:35:26 +0300

    JavaFX Override

---
+++ src/project/MainMenuController.java
@@ -0,0 +1,45 @@
+package project;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+
+public class MainMenuController {
+
+    public TextField IPTextField;
+
+    public void startGame(ActionEvent ae){
+        System.out.println("GAME STARTED");
+
+        Alert alert = new Alert(Alert.AlertType.NONE, "GAME IS STARTING", ButtonType.CLOSE);
+        alert.showAndWait();
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("WaitScreen.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void joinGame(ActionEvent ae){
+        if (IPTextField.getText().equals("")) {
+            System.out.println("Enter a URL, dummy..");
+            Alert alert = new Alert(Alert.AlertType.NONE, "Enter a URL, dummy..", ButtonType.CLOSE);
+            alert.showAndWait();
+            return;
+        } else{
+            System.out.println("JOIN THE GAME!!");
+            Alert alert = new Alert(Alert.AlertType.NONE, "JOIN THE GAME!!", ButtonType.CLOSE);
+            alert.showAndWait();
+
+        }
+    }
+}
