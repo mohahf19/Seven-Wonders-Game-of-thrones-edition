b373d81cfc4b67120f9a0178f7de6fb92e151fc2
Mohamad Fakhouri <mohahf19@gmail.com>
2019-11-18 12:23:27 +0300

    added buttons

--- src/backend/controllers/WaitScreenController.java
+++ src/backend/controllers/WaitScreenController.java
@@ -1,4 +1,12 @@
 package backend.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class WaitScreenController {
+    @FXML
+    private Button exitGameButton;
+
+    @FXML
+    private Button startGameButton;
 }
