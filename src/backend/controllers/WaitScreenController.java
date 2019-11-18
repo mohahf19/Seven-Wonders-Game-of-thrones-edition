fbb8c1c7b2ec0eaed34a4e6dcbc3a20f8b77187a
Mohamad Fakhouri <mohahf19@gmail.com>
2019-11-18 23:11:01 +0300

    help

--- src/backend/controllers/WaitScreenController.java
+++ src/backend/controllers/WaitScreenController.java
@@ -4,15 +4,19 @@ import backend.app.fxmlPaths;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class WaitScreenController{
+public class WaitScreenController implements Initializable {
     @FXML
     private Button exitGameButton;
 
@@ -20,15 +24,41 @@ public class WaitScreenController{
     private Button startGameButton;
 
     @FXML
-    private Label house1, house2, house3, house4, house5, house6, house7;
+    private static Label house0, house1, house2, house3, house4, house5, house6;
+
+    @FXML
+    private static ImageView image0, image1, image2, image3, image4, image5, image6;
+
+    public static ArrayList<Label> labels = new ArrayList<>();
+
+    public static ArrayList<ImageView> images = new ArrayList<>();
 
     @FXML
     private void startGame(ActionEvent ae){
-        house1.setText( "ABCDDDDD");
+
     }
 
     public static void updateHouses(ArrayList<String> houses){
+        labels.clear();
+        images.clear();
+        labels.add(house0);
+        labels.add(house1);
+        labels.add(house2);
+        labels.add(house3);
+        labels.add(house4);
+        labels.add(house5);
+        labels.add(house6);
 
+        images.add(image0);
+        images.add(image1);
+        images.add(image2);
+        images.add(image3);
+        images.add(image4);
+        images.add(image5);
+        images.add(image6);
+        for(int i = 0; i < houses.size(); i++){
+            labels.get(i).setText(houses.get(i));
+        }
     }
 
     @FXML
@@ -42,5 +72,10 @@ public class WaitScreenController{
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
