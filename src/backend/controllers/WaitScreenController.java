b7e109e31962faa12671df23e6aca6e5ad4b3935
Mohamad Fakhouri <mohahf19@gmail.com>
2019-11-18 23:53:23 +0300

    wait screen background

--- src/backend/controllers/WaitScreenController.java
+++ src/backend/controllers/WaitScreenController.java
@@ -10,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import javafx.scene.image.ImageView;
