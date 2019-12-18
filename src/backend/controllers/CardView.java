1eb878045dcbe2e450d09f224d99739720b6a4a3
Muhammad Bilal Bin Khalid <48215073+Bilalbink@users.noreply.github.com>
2019-12-18 23:46:28 +0300

    temp

--- src/backend/controllers/CardView.java
+++ src/backend/controllers/CardView.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 
