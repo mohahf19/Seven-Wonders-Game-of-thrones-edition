7e3b91fdc4ad0afc50bf02bbedf42eb3ca342138
Muhammad Arham Khan <muhammadarhamkhan@gmail.com>
2019-12-26 00:40:03 +0300

    ui update issue fixed

--- src/backend/controllers/PlayScreenController.java
+++ src/backend/controllers/PlayScreenController.java
@@ -18,6 +18,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import javax.swing.*;
 import java.io.IOException;
